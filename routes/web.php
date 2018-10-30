cb44ea5290cf5114faf9b8925ed9a3701df0e836
Malte Riechmann <kontakt@malteriechmann.de>
2018-10-30 20:54:13 +0100

    [CLEANUP] Add a space after the function keyword, make PSR-2 compatible

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ Route::get('/', 'GodparenthoodController@index');
 Route::get('/suche', 'SearchController@search');
 Route::get('/statistiken', 'DebitEntryController@statistics');
 Route::get('/datenbank-dump', 'Controller@generateDbDump');
-Route::get('/patenschaften', function() {
+Route::get('/patenschaften', function () {
     return redirect()->action('GodparenthoodController@index');
 });
 Route::get('/patenschaften/neu', 'GodparenthoodController@create');
 Route::post('/patenschaften/erstellen', 'GodparenthoodController@store');
 Route::get('/patenschaften/{godparenthood}', 'GodparenthoodController@show');
-Route::get('/kinder', function() {
+Route::get('/kinder', function () {
     return redirect()->action('GodparenthoodController@index');
 });
 Route::get('/kinder/neu', 'ChildController@create');
